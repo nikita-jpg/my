1dd5729cb893c9273b3818e7f0db3fa151224024
nikita <nicitazaletin@yandex.ru>
2019-11-02 01:27:26 +0300

    Dz1810Pendulum

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new Dz189ModelSolarSystem(this));
+        setContentView(new Dz1810Pendulum(this));
     }
 }
