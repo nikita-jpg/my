ef2fccbe3ee01d7b09d26c45d862179a3c94f5df
nikita <nicitazaletin@yandex.ru>
2019-11-01 14:18:15 +0300

    Dz187

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new Dz177LinePicture(this));
+        setContentView(new Dz187Animation(this));
     }
 }
