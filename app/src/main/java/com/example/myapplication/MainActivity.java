dfe8197d7d927351631bb77d17adf4afce7dc50f
nikita <nicitazaletin@yandex.ru>
2019-11-02 14:43:12 +0300

    Dz196Func

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new Dz1811(this));
+        setContentView(new Dz196Func(this));
     }
 }
