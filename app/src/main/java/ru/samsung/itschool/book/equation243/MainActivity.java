15b8fd8442c3645abc6ab40b900d0100f6c77a16
nikita <nicitazaletin@yandex.ru>
2019-11-17 01:34:30 +0300

    dz252

--- app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
@@ -2,40 +2,44 @@ package ru.samsung.itschool.book.equation243;
 
 import android.app.Activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  {
     // Вызывается при создании Активности
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Инициализирует Активность.
         setContentView(R.layout.activity_main);
+        final EditText name= (EditText) findViewById(R.id.name);
+        final EditText password= (EditText) findViewById(R.id.password);
+        final TextView result = (TextView) findViewById(R.id.result);
+        Button button1=(Button) findViewById(R.id.button7);
+
+        View.OnClickListener onClickListener =new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId()==R.id.button7) {
+                    if ( (password.getText().toString()).equals("toor")) {
+                        result.setTextColor(Color.GREEN);
+                        result.setText("Верно");
+
+                    } else {
+                        result.setTextColor(Color.RED);
+                        result.setText("Вы ошиблись в логине или пароле");
+                        name.setText("");
+                        password.setText("");
+                    }
+                }
+            }
+        };
+        button1.setOnClickListener(onClickListener);
+
     }
-
-    /** Вызывается при нажатии пользователем на кнопку Решить */
-        public void solveEquation(View view) {
-             // ax+b=c
-            double a = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_a)).getText().toString());
-            double b = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_b)).getText().toString());
-            double c = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_c)).getText().toString());
-            TextView result = (TextView) findViewById(R.id.result);
-            double D=b*b-4*a*c;
-            if(0<D){
-                result.setText("X1 = " + String.valueOf((double)(-b-Math.sqrt(D))/(2*a)) +"\n"+"X2 = " + String.valueOf((double)(-b+Math.sqrt(D))/(2*a))  );
-
-            }
-            else if(D==0){
-                result.setText("X = " + String.valueOf(  (double)(-b/(2*a)) ));
-            }
-            else result.setText("No ruts");
-
-        }
-
 }
